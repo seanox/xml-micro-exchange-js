0eb121a0d56c042a5fa0e79b4fa68c328186347f
seanox <service@seanox.com>
2024-11-04 21:53:42 +0100

    #0000 Service: Review and optimization

--- sources/service.js
+++ sources/service.js
@@ -31,9 +31,12 @@
  *     TERMS / WORDING
  * TODO:
  */
-import http from "http";
-import https from "https";
-import fs from "fs";
+import http from "http"
+import https from "https"
+import fs from "fs"
+import path from "path"
+
+import Mime from "mime/lite"
 
 // For the environment variables, constants are created so that they can be
 // assigned as static values to the constants in the class!
@@ -42,8 +45,8 @@ class Runtime {
     static getEnv(variable, standard) {
         if (!process.env.hasOwnProperty(variable)
                 || String(process.env[variable]).trim().length <= 0)
-            return standard;
-        return String(process.env[variable]).trim();
+            return standard
+        return String(process.env[variable]).trim()
     }
 }
 
@@ -64,6 +67,7 @@ const XMEX_REQUEST_XPATH_DELIMITER = Runtime.getEnv("XMEX_REQUEST_XPATH_DELIMITE
 
 const XMEX_CONTENT_DIRECTORY = Runtime.getEnv("XMEX_CONTENT_DIRECTORY", "./content")
 const XMEX_CONTENT_DEFAULT = Runtime.getEnv("XMEX_CONTENT_DEFAULT", "index.html openAPI.html")
+const XMEX_CONTENT_REDIRECT = Runtime.getEnv("XMEX_CONTENT_REDIRECT")
 
 const XMEX_STORAGE_DIRECTORY = Runtime.getEnv("XMEX_STORAGE_DIRECTORY", "./data")
 const XMEX_STORAGE_SPACE = Runtime.getEnv("XMEX_STORAGE_SPACE", "256K")
@@ -85,6 +89,36 @@ Number.parseBytes = function(text) {
     let number = Number.parseFloat(match[1])
     let factor = ("kmg").indexOf(match[2]) +1
     return number *Math.pow(1024, factor)
+}
+
+// Query if something exists, it minimizes the check of undefined and null
+Object.exists = function(object) {
+    return object !== undefined && object !== null
+}
+
+http.ServerResponse.prototype.quit = function(status, message, headers = undefined, data = undefined) {
+
+    if (this.headersSent)
+        return
+
+    // Directly set headers (e.g. for CORS) are read
+    // Names are converted to camel case, pure cosmetics -- Node.js uses only lower case
+    if (typeof headers !== "object")
+        headers = {}
+
+    for (const [header, value] of Object.entries(this.getHeaders()))
+        headers[header.replace(/\b[a-z]/g, match =>
+            match.toUpperCase())] = value
+
+    this.writeHead(status, message, headers)
+    if (Object.exists(data))
+        this.contentLength = data.length
+    this.end(data)
+}
+
+http.ServerResponse.prototype.exit = function(status, message, headers = undefined, data = undefined) {
+    this.quit(status, message, headers, data)
+    throw http.ServerResponse.prototype.exit
 }
 
 class Storage {
@@ -151,12 +185,88 @@ class ServerFactory {
 
     static STORAGE_SPACE = Number.parseBytes(XMEX_STORAGE_SPACE)
 
+    static CONTENT_DIRECTORY = XMEX_CONTENT_DIRECTORY
+    static CONTENT_DEFAULT = XMEX_CONTENT_DEFAULT
+    static CONTENT_REDIRECT = XMEX_CONTENT_REDIRECT
+
     static serverInstancesAcme = undefined
     static serverInstancesXmex = undefined
 
+    static onContentRequest(request, response) {
+
+        // Very simple/rudimentary implemented web server. Supported are the
+        // methods OPTIONS/HEAD/GET. This way, content can be provided outside
+        // the URL of the API, e.g. for documentation.
+
+        const method = request.method.toUpperCase()
+        if (method === "OPTIONS")
+            response.exit(204, "No Content", {Allow: "OPTIONS, HEAD, GET"})
+
+        if (ServerFactory.CONTENT_REDIRECT) {
+            let location = ServerFactory.CONTENT_REDIRECT
+            if (location.match(/\.{3,}$/))
+                location = location.replace(/\.{3,}$/, request.url)
+            response.exit(301, "Moved Permanently", {Location: location})
+        }
+
+        if (!ServerFactory.CONTENT_DIRECTORY)
+            response.exit(404, "Resource Not Found")
+
+        let target = path.normalize(decodeURI(request.url.replace(/\?.*$/, "")).trim()).replace(/\\+/g, "/")
+        target = ServerFactory.CONTENT_DIRECTORY + "/" + target
+        target = target.replace(/\/{2,}/g, "/")
+        if (!fs.existsSync(target))
+            response.exit(404, "Resource Not Found")
+        if (!fs.lstatSync(target).isFile()
+                && !fs.lstatSync(target).isDirectory())
+            response.exit(404, "Resource Not Found")
+
+        if (fs.lstatSync(target).isDirectory()) {
+            if (ServerFactory.CONTENT_DEFAULT) {
+                let files = ServerFactory.CONTENT_DEFAULT.split(/\s+/)
+                while (files.length > 0) {
+                    if (!files[0].includes("/")
+                            && !files[0].includes("\\")
+                            && fs.existsSync(target + "/" + files[0])
+                            && fs.lstatSync(target + "/" + files[0]).isFile())
+                        break
+                    files.shift()
+                }
+                if (files.length <= 0)
+                    response.exit(403, "Forbidden")
+                target += files[0]
+            } else response.exit(403, "Forbidden")
+        }
+
+        if (!["OPTIONS", "HEAD", "GET"].includes(method))
+            response.exit(405, "Method Not Allowed", {Allow: "OPTIONS, HEAD, GET"})
+
+        target = fs.realpathSync(target)
+        const state = fs.lstatSync(target)
+        const headers = {
+            "Content-Length": state.size,
+            "Content-Type": Mime.getType(target),
+            "Last-Modified": new Date(state.mtimeMs).toUTCString()
+        }
+        if (method === "HEAD")
+            response.exit(200, "Success", headers)
+        let file = fs.openSync(target, "r")
+        try {
+            response.writeHead(200, "Success"/*, headers*/)
+            response.contentLength = state.size || 0
+            const buffer = Buffer.alloc(65535)
+            for (let size = 0; size = fs.readSync(file, buffer) > 0;)
+                response.write(buffer.toString("binary"), "binary")
+            response.end()
+        } finally {
+            fs.closeSync(file)
+        }
+        throw http.ServerResponse.prototype.exit
+    }
+
     static newInstance() {
 
-        const secure = ServerFactory.CONNECTION_CERTIFICATE && ServerFactory.CONNECTION_SECRET;
+        const secure = ServerFactory.CONNECTION_CERTIFICATE && ServerFactory.CONNECTION_SECRET
         const context = secure ? https : http
         const options = secure ? (() => {
             if (fs.existsSync(ServerFactory.CONNECTION_CERTIFICATE)) {
@@ -210,15 +320,53 @@ class ServerFactory {
             })
 
             request.on("end", () => {
+
+                try {
+
+                    // Marking the start time for request processing
+                    request.timing = new Date().getTime()
+                    request.data = Object.exists(request.data) ? request.data : ""
+
+                    if (Object.exists(request.error))
+                        response.exit(...request.error)
+
+                    const requestUrl = decodeURI(request.url)
+                    if (ServerFactory.ACME_CHALLENGE
+                            && ServerFactory.ACME_TOKEN
+                            && requestUrl.toLowerCase() === ServerFactory.ACME_CHALLENGE.toLowerCase())
+                        response.exit(200, "Success", {"Content-Length": ServerFactory.ACME_TOKEN.length}, ServerFactory.ACME_TOKEN)
+
+                    // The API should always use a context path so that the
+                    // separation between URI and XPath is also visually
+                    // recognizable. For all requests outside of the API path
+                    // are alternatively considered as a request to web content.
+                    // If content has been configured, it will be used like a
+                    // normal HTTP server. Otherwise the requests are answered
+                    // with status 404.
+
+                    if (!decodeURI(request.url).startsWith(ServerFactory.CONNECTION_CONTEXT))
+                        ServerFactory.onContentRequest(request, response)
+
+                } catch (error) {
+                    if (error === Storage.prototype.exit
+                            || error === http.ServerResponse.prototype.exit)
+                        return
+                    const unique = Date.now().toString(36).toUpperCase()
+                    console.error("Service", "#" + unique, error)
+                    if (!response.headersSent)
+                        response.quit(500, "Internal Server Error", {"Error": "#" + unique})
+                } finally {
+
+                }
             })
         })
 
-        ServerFactory.on("error", (error) => {
+        server.on("error", (error) => {
             console.error(error.stack || error)
             process.exit(1)
         })
 
-        ServerFactory.listen(ServerFactory.CONNECTION_PORT, ServerFactory.CONNECTION_ADDRESS, function() {
+        server.listen(ServerFactory.CONNECTION_PORT, ServerFactory.CONNECTION_ADDRESS, function() {
             let options = []
             if (ServerFactory.CONNECTION_CERTIFICATE
                     && ServerFactory.CONNECTION_SECRET)
@@ -254,23 +402,23 @@ class ServerFactory {
                 } catch (error) {
                     if (error === http.ServerResponse.prototype.exit)
                         return
-                    const unique = Date.now().toString(36).toUpperCase();
+                    const unique = Date.now().toString(36).toUpperCase()
                     console.error("Service", "#" + unique, error)
                     if (!response.headersSent)
                         response.quit(500, "Internal Server Error", {"Error": "#" + unique})
                 }
             })
 
-            ServerFactory.on("error", (error) => {
+            ServerFactory.serverInstancesAcme.on("error", (error) => {
                 console.error(error.stack || error)
                 process.exit(1)
             })
 
-            ServerFactory.listen(80, ServerFactory.CONNECTION_ADDRESS, function() {
+            ServerFactory.serverInstancesAcme.listen(80, ServerFactory.CONNECTION_ADDRESS, function() {
                 console.log("Service", `Listening at ${this.address().address}:${this.address().port} (ACME)`)
             })
         }
     }
 }
 
-ServerFactory.bind();
+ServerFactory.bind()
